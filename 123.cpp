aa1a261963c0fcc61efac490ed5446df8a2f8e46
alhusseindev <alhussein.tarek97@gmail.com>
2021-04-24 21:02:15 +0200

    My 123 File

---
+++ 123.cpp
@@ -0,0 +1,18 @@
+#include <climits>
+#include <iostream>
+
+
+int main(){
+    int a;
+
+    std::cin >> a;
+
+    while(std::cin.fail()){
+        std::cout << "Error, try again please" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(INT_MAX, '\n');
+        //ask the user for input
+        std::cout << "Enter a number" << std::endl;
+        std::cin >> a;
+    }    
+}
